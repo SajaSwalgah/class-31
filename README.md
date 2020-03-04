e2ca27caa93a0bd92c2124e6b63d5b3117f8ff8f
SajaSwalgah <saja.swalgah@yahoo.com>
2020-03-04 22:18:49 +0200

    netlify deploy

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# class-32
+#  LAB - Class 31
+
+## Hooks API
+
+### Author: Saja Swalgah
+
+### Links and Resources
+
+- [submission PR](https://github.com/Saja-401-advanced-javascript/class-28/pull/1)
+- [Netlify Deploy](https://angry-kalam-1e50e8.netlify.com/)
+
+
+
+
+#### How to initialize/run your application (where applicable)
+
+-`npm run start` || `nodemon`
+
+
+#### UML
+
+![](img/class-31.jpg)
--- src/components/main.js
+++ src/components/main.js
@@ -1,55 +1,96 @@
 import React, { useEffect, useState } from 'react';
+// import uuid from 'uuid';
 
-let arr =[];
 
 function Main(props) {
     const [toDo, setToDo] = useState([])
     const [item, setItem] = useState('');
     const [assignee, setAssignee] = useState('');
-    const [status, setStatus] = useState('');
-    const [difficulty, setDifficulty] = useState('');
+    const [status, setStatus] = useState('incomplete');
+    const [difficulty, setDifficulty] = useState(3);
+
+    // let id = uuid;
 
 
     const _addItem = (e) => {
         setItem(e.target.value);
     }
     const _addAssignee = (e) => {
-      setAssignee(e.target.value);
-      let x= e.target.value
-      arr.push(x)
-      console.log('kkkkkk', arr);     
+        setAssignee(e.target.value);
+    }
+    const _addStatus = (e) => {
+        setStatus('completed')
     }
 
-    
-    const _addStatus = (e) => {
-        setStatus(e.target.value);
-    }
     const _addDiff = (e) => {
         setDifficulty(e.target.value);
+
     }
-    
+
+
+
+
+
+
     const _addToDo = (e) => {
+
+
+
+        setStatus('incomplete')
         e.preventDefault();
         e.target.reset();
-        setToDo([item, assignee]);
-        // setS([status, difficulty])
-        // console.log('rrrrrrrrr', setToDo([item, assignee]));        
+        setToDo([...toDo, [item, assignee, status]]);
+        console.log('rrrrrrrrr', difficulty);
+
     }
-    
+
+
+    useEffect(() => {
+
+        if (toDo.length >= 1) {
+            let ctr = 0;
+            let cctr = 0
+            // console.log('rrrrrrrrrrrrrrrrrrrrr', toDo);
+            for (let i = 0; i < toDo.length; i++) {
+                if (toDo[i][2] === 'incomplete') {
+                    ctr++
+                } else if (toDo[i][2] === 'complete') {
+                    cctr++
+                }
+            }
+            document.title = `${ctr} items incompleted and ${cctr} items have completed`;
+        } else document.title = `There Is No List!`;
+    }, [toDo]);
+
+    const changeStatus = e => {
+        for (let i = 0; i < toDo.length; i++) {
+            console.log('888888888', e.target.name);
+            if (toDo[i][0] === e.target.name && toDo[i][2] === 'incomplete') {
+                setStatus('completed')
+            } else if (toDo[i][2] === 'completed') {
+                setStatus('incomplete')
+            }
+        }
+    }
+
+
 
 
     return (
         <section>
             <form onSubmit={_addToDo}>
-                <input onChange={_addItem} placeholder="to do" type="text" className="inputs"/><br />
+                <input onChange={_addItem} placeholder="to do" type="text" className="inputs" /><br />
                 <input onChange={_addAssignee} placeholder="assignee" type="text" className="inputs" /><br />
-                <input onChange={_addDiff} type="range" placeholder="difficuly from 1-5" max="5" min="0" className="inputs" defaultValue="3"/> <br />
+                <label>
+                    <input onChange={_addDiff} type="range" placeholder="difficuly from 1-5" max="5" min="0" className="inputs" defaultValue="3" /> <br />
+                    {/* <span> Difficulty</span> */}
+                </label>
                 <label className="container">
-                    <input onChange={_addStatus} type="checkbox" />
-                   <span id="state"> State</span> 
+                    <input onClick={_addStatus} type="checkbox" />
+                    <span id="state"> State</span>
                     <span className="checkmark"></span>
                 </label>
-                
+
 
                 {/* <input onChange={_addStatus} type="checkbox" id="chek" />
                 <label>status </label> */}
@@ -57,20 +98,22 @@ function Main(props) {
             </form>
             <p id="title"> To Do Items  </p>
             {
-                toDo.map(item =>
-                    <p key={item} id="items">item :{item}</p>
-                    // let arr =[]
-                    // arr.push(item)
+                toDo.map((item, idx) =>
+                    <li key={idx} id="items">
+                        TO DO : {item[0]}
+                        <span id="assignee">By : {item[1]} </span>
+                        <button onClick={changeStatus} name={item[0]} style={{ background: item[2] === 'incomplete' ? 'red' : 'greenF' }}>{item[2]}</button>
 
+                    </li>
                 )
             }
-
         </section>
     )
 
 }
 
-console.log(arr );
+//  "homepage": "https://github.com/SajaSwalgah/class-31/",
+
 
 
 export default Main
